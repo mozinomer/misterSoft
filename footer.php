d6ee14ec2fa78d5c81792bcc72f24f0eabbb67f5
mozinomer <mozinomer12@gmail.com>
2021-04-12 16:07:10 +0500

    blank theme added

---
+++ footer.php
@@ -0,0 +1,74 @@
+<footer>
+		<div class="container">
+			<div class="innerFooterContainer">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="leftCOntainerFooeoris">
+							<div class="logoContainerFooter">
+								<img src="./images/2.svg">
+							</div>
+							<p>
+								Mister Softee sells soft serve ice cream sundaes, soda floats, cones, cups, banana
+								boats, and shakes from a brand new state of the art Ice Cream truck. We are based out of
+								Redwood City, CA.
+							</p>
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="menuContainerFooter">
+							<div class="innerMenuCOntianerFOoter">
+								<h6>Quick Links</h6>
+								<ul>
+									<li><a href="index.html">Home</a></li>
+									<li><a href="gift-card.html">Gift Cards</a></li>
+									<li><a href="faqs.html">FAQ</a></li>
+									<li><a href="request.html">Request a Weekly Softee Stop</a></li>
+									<li><a href="our-menu.html">Our Menu</a></li>
+									<li><a href="suggestion.html">Suggestions</a></li>
+								</ul>
+							</div>
+							<div class="innerMenuCOntianerFOoter">
+								<h6>Social Media</h6>
+								<ul>
+									<li><a href="#">Facebook</a></li>
+									<li><a href="#">Twitter</a></li>
+									<li><a href="#">Instagram</a></li>
+									<li><a href="#">Youtube</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="contentContainerFooter">
+							<p>Call today at <a href="#">888-587-6383</a> or book now to have Mister Softee attend your
+								next event!</p>
+							<a href="#" class="bookNow">BOOK NOW</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyRIghts">
+				<p>© Copyright 2021 Mister Softee. All rights reserved. </p>
+				<ul>
+					<li><a href="#">Terms & Conditions | </a></li>
+					<li><a href="#">Privacy Policy</a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+
+
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"
+	integrity="sha512-gY25nC63ddE0LcLPhxUJGFxa2GoIyA5FLym4UJqHDEMHjp8RET6Zn/SHo1sltt3WuVtqfyxECP38/daUc/WVEA=="
+	crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/shh.js"></script>
+
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>INdex</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- addding the cdn's -->
+	<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+		crossorigin="anonymous"></script>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+		integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css"
+		integrity="sha512-UTNP5BXLIptsaj5WdKFrkFov94lDx+eBvbKyoe1YAfjeRPC+gT5kyZ10kOHCfNZqEui1sxmqvodNUx3KbuYI/A=="
+		crossorigin="anonymous" />
+	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css"
+		integrity="sha512-H9jrZiiopUdsLpg94A333EfumgUBpO9MdbxStdeITo+KEIMaNfHNvwyjjDJb+ERPaRS6DpyRlKbvPUasNItRyw=="
+		crossorigin="anonymous" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"
+		integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA=="
+		crossorigin="anonymous"></script>
+	<!-- <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+	<script src="https://alexandrebuffet.fr/codepen/slider/slick-animation.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.12/jquery.mousewheel.js"></script>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/media.css">
+</head>
+
+<body <?php echo body_class(); ?>>
+	<header>
+		<div class="container">
+			<div class="innerContainerHeader">
+				<div class="hamburger">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div class="logoCCOntainerHeader">
+					<img src="./images/2.svg">
+				</div>
+				<div class="listContainerLi">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="gift-card.html">Gift Cards</a></li>
+						<li><a href="faqs.html">FAQ</a></li>
+						<li><a href="request.html">Request a Weekly Softee Stop</a></li>
+						<li><a href="our-menu.html">Our Menu</a></li>
+						<li><a href="blogs.html">News</a></li>
+						<li><a href="request.html">Contact Us</a></li>
+						<li class="last-child"><a href="book.html">Book Softee</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
